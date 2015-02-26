7de8274e5a287fdd86026e2aa93676d105d50503
Franck Chauvel <franck.chauvel@gmail.com>
2015-02-26 10:06:44 +0100

    Adding missing license headers

--- library/src/main/java/org/mutable/storage/csv/CSVFormat.java
+++ library/src/main/java/org/mutable/storage/csv/CSVFormat.java
@@ -3,17 +3,18 @@
  *
  * Copyright (C) 2015 Franck Chauvel <franck.chauvel@gmail.com>
  *
- * MuTable is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
+ * MuTable is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Mutable is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * Mutable is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with MuTable. If not, see <http://www.gnu.org/licenses/>.
+ * along with MuTable.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.mutable.storage.csv;
 
